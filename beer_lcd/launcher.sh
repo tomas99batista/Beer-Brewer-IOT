e9b4d3726c22dec21a7dcaaae76c691095f6d730
tomas99batista <tomas99batista@gmail.com>
2020-12-07 14:21:33 +0000

    made run on startup

---
+++ beer_lcd/launcher.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /
+cd home/pi/Desktop/Beer-Temp-Watcher/beer_lcd/
+sudo python3 lcd_beer_temp.py
